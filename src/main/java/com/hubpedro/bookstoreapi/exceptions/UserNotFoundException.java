ff85425656bad51dbd36380edc0eee7aaafed38f
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-10-30 22:38:21 -0300

    refactor: Improves UserNotFoundException to return 404

--- src/main/java/com/hubpedro/bookstoreapi/exceptions/UserNotFoundException.java
+++ src/main/java/com/hubpedro/bookstoreapi/exceptions/UserNotFoundException.java
@@ -2,7 +2,9 @@ package com.hubpedro.bookstoreapi.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends ErrorResponseException {
 
 	public UserNotFoundException(String message) {
