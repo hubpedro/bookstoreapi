72cdacdfdcb28663b43e3eec75ee71d4bc35e562
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-10-30 22:36:58 -0300

    refactor: Improves DomainValidateException to return 400

--- src/main/java/com/hubpedro/bookstoreapi/exceptions/DomainValidateException.java
+++ src/main/java/com/hubpedro/bookstoreapi/exceptions/DomainValidateException.java
@@ -1,7 +1,13 @@
 package com.hubpedro.bookstoreapi.exceptions;
 
-public class DomainValidateException extends RuntimeException {
-	public DomainValidateException(String message) {
-		super(message);
-	}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class DomainValidateException extends ErrorResponseException {
+
+    public DomainValidateException(String reason) {
+        super(HttpStatus.BAD_REQUEST);
+    }
 }
