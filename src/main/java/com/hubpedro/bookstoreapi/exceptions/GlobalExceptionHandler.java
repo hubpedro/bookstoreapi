9e45bca15654b55b45f63e142f9cd8417618a38f
Pedro Barbosa <pedro.barbosa.dev@gmail.com>
2025-10-30 22:38:11 -0300

    feat: Add exception handlers for domain validation and illegal state

--- src/main/java/com/hubpedro/bookstoreapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/hubpedro/bookstoreapi/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.hubpedro.bookstoreapi.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,4 +25,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 		return ResponseEntity.status(ex.getStatusCode()).body(ex.getBody());
 	}
+
+    @ExceptionHandler(DomainValidateException.class)
+    public ResponseEntity<ProblemDetail> handleDomainValidateException(DomainValidateException ex, WebRequest request) {
+        return ResponseEntity.status(ex.getStatusCode()).body(ex.getBody());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ProblemDetail> handleIllegalStateException(IllegalStateException ex, WebRequest request) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, ex.getMessage());
+        problemDetail.setTitle("Loan Already Returned");
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(problemDetail);
+    }
 }
